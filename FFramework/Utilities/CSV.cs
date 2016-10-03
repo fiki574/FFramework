f606a65d409d8d6be6a793c1cab7308fd2aa7fdd
fiki574 <fiki8796@hotmail.com>
2016-10-03 22:53:22 +0200

    Male adicije...

---
+++ FFramework/Utilities/CSV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FFramework.Utilities
+{
+    public class CSV
+    {
+        public static ThreadSafeList<String[]> GetFileContent(string csv_file)
+        {
+            ThreadSafeList<String[]> all = new ThreadSafeList<String[]>();
+            try
+            {
+                if (!System.IO.File.Exists(csv_file)) return null;
+                StreamReader f = new StreamReader(csv_file, Encoding.Default);
+                string line;
+                while ((line = f.ReadLine()) != null) all.Add(line.Split(new char[] { ';' }));
+            }
+            catch
+            {
+                return null;
+            }
+            return all;
+        }
+    }
+}
--- FFramework/Utilities/Date.cs
+++ FFramework/Utilities/Date.cs
@@ -41,5 +41,21 @@ namespace FFramework.Utilities
             else if (month == 2) return IsLeapYear(year) == true ? 29 : 28;
             else return 0;
         }
+
+        public static bool IsDigitsOnly(string str, bool allow_dot = false)
+        {
+            if (!allow_dot)
+                foreach (char c in str)
+                {
+                    if (c < '0' || c > '9')
+                        return false;
+                }
+            else
+                foreach (char c in str)
+                    if (c < '0' || c > '9')
+                        if (c != '.')
+                            return false;
+            return true;
+        }
     }
 }
